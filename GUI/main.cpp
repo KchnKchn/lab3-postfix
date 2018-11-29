b7dcf6e5cd7a744435cbb7b47a73d4376352a32e
KchnKchn <khvatov.alexander@gmail.com>
2018-11-29 09:31:59 +0300

    Add GUI

---
+++ GUI/main.cpp
@@ -0,0 +1,12 @@
+#include "gui.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+void Main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	GUI::gui form;
+	Application::Run(%form);
+}
--- cmake/function.cmake
+++ cmake/function.cmake
@@ -3,13 +3,13 @@ function(create_project_lib TARGET )
     file(GLOB TARGET_HD "*.h*")
     add_library(${TARGET} STATIC ${TARGET_SRC} ${TARGET_HD})
     
-    get_property ( INCLUDE_DIRS GLOBAL PROPERTY INC_DIR)
+    get_property(INCLUDE_DIRS GLOBAL PROPERTY INC_DIR)
     list(APPEND INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR})
-    set_property ( GLOBAL PROPERTY INC_DIR ${INCLUDE_DIRS})
+    set_property(GLOBAL PROPERTY INC_DIR ${INCLUDE_DIRS})
     
-    get_property ( LIB_LIST GLOBAL PROPERTY LIBS_P)
+    get_property(LIB_LIST GLOBAL PROPERTY LIBS_P)
     list(APPEND LIB_LIST ${TARGET})
-    set_property ( GLOBAL PROPERTY LIBS_P ${LIB_LIST})
+    set_property(GLOBAL PROPERTY LIBS_P ${LIB_LIST})
     
 endfunction()
 
@@ -18,10 +18,26 @@ function(create_executable_project TARGET)
     file(GLOB TARGET_HD "*.h*")
     add_executable(${TARGET} ${TARGET_SRC} ${TARGET_HD})
     
-    get_property ( INCLUDE_DIRS GLOBAL PROPERTY INC_DIR)
+    get_property(INCLUDE_DIRS GLOBAL PROPERTY INC_DIR)
     target_include_directories(${TARGET} PUBLIC ${INCLUDE_DIRS})
     
-    get_property ( LIB_LIST GLOBAL PROPERTY LIBS_P)
+    get_property(LIB_LIST GLOBAL PROPERTY LIBS_P)
+    target_link_libraries(${TARGET} ${LIB_LIST})
+    
+endfunction()
+
+function(create_clr_executable_project TARGET)
+    file(GLOB TARGET_SRC "*.cpp") 
+    file(GLOB TARGET_HD "*.h*")
+    add_executable(${TARGET} ${TARGET_SRC} ${TARGET_HD})
+    
+    get_property(INCLUDE_DIRS GLOBAL PROPERTY INC_DIR)
+    target_include_directories(${TARGET} PUBLIC ${INCLUDE_DIRS})
+    
+    get_property(LIB_LIST GLOBAL PROPERTY LIBS_P)
+    
+    set_property(TARGET ${TARGET} PROPERTY COMMON_LANGUAGE_RUNTIME "clr")
+    
     target_link_libraries(${TARGET} ${LIB_LIST})
     
 endfunction()
